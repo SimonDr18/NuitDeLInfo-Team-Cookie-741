226b8e3c84ecbbc0482a935f2419b8bda15f4374
aubry <geekyready@gmail.com>
2017-12-08 00:31:20 +0100

    creerevent.php

---
+++ includes/creerevenement.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>[titre]</title>
+  </head>
+
+  <header>
+    <h1>Creer un evenement</h1>
+  </header>
+
+  <section>
+    <form action="#" method="post">
+      Titre evenement : <input type="text" name="event" /><br />
+      Type : <input type="text" name="type" /><br />
+      Nombre de blessés : <input type="text" name="nbblesses" /><br />
+      <input type="submit" value="Creer"/>
+    </form>
+</html>
