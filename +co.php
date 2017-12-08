5b8c2cc7020e01e9535c414d3eabf4ed9910c1ec
GUICHARD Clement <clement.guichard1@etu.univ-orleans.fr>
2017-12-08 03:58:21 +0100

    connexion ok

--- +co.php
+++ +co.php
@@ -4,7 +4,20 @@ session_start();
 
 if(isset($_REQUEST["Pseudo"]) AND isset($_REQUEST["Pass"])){
   if(isset($file_db)){
-    echo "YATTA";
+    $req = "SELECT idU, login, mdp FROM MEMBRE WHERE login=:login AND mdp=:mdp GROUP BY idU ORDER BY prenom, nom";
+    $stmt = $file_db->prepare($req);
+    $stmt->bindParam(":login", $_REQUEST["Pseudo"]);
+    $stmt->bindParam(":mdp", $_REQUEST["Pass"]);
+    $stmt->execute();
+    $data = $stmt->fetch();
+    if(isset($data['idU'])){
+      $_SESSION["idU"]=$data['idU'];
+      $_SESSION["login"]=$data['login'];
+      $_SESSION["mdp"]=$data['mdp'];
+      header("Location: index.php");
+    } else {
+      header("Location: connexion.php?err=1");
+    }
   } else {
     header("Location: 404.php");
   }
--- connexion.php
+++ connexion.php
@@ -13,13 +13,20 @@ $_SESSION["page"]="connexion.php";
   <body>
     <?php require_once("includes/_header.php"); ?>
     <header>
-      <h1>connexion</h1>
+      <h1>Connexion</h1>
     </header>
     <section>
+      <?php
+        if(isset($_REQUEST["err"]))
+          echo "Utilisateur inconnu";
+      ?>
       <form action="+co.php" method="post">
-        Pseudo : <input type="text" name="Pseudo"/><br />
+        Login : <input type="text" name="Pseudo"/><br />
         Mot de passe : <input type="password" name="Pass" /><br />
         <input type="submit" value="Connexion"/>
+      </form>
+      <form action="inscription.php" style="margin: 5%" method="post">
+        <input type="submit" value="inscription"/>
       </form>
     </section>
   </body>
