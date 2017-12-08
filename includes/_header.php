298fb971ed45a8dab6909be0feb33d7332b00ff2
Heloise Coulon <coulon@info27-20.iut45.univ-orleans.fr>
2017-12-08 02:38:10 +0100

    changement hearder

--- includes/_header.php
+++ includes/_header.php
@@ -7,25 +7,25 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a <?php if($_SESSION["page"]=="index.php"){ echo "class='active'"; } ?> class="navbar-brand" href="index.php">Cookies Prévention</a>
+            <a class="navbar-brand" href="index.php">Cookies Prévention</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a <?php if($_SESSION["page"]=="soiree.php"){ echo "class='active'"; } ?> href="soiree.php">Creer soirée</a></li>
-                <li><a <?php if($_SESSION["page"]=="creerevenement.php"){ echo "class='active'"; } ?> href="creerevenement.php">Signaler évenement</a></li>
+                <li <?php if($_SESSION["page"]=="soiree.php"){ echo "class='active'"; } ?>><a  href="soiree.php">Creer soirée</a></li>
+                <li <?php if($_SESSION["page"]=="creerevenement.php"){ echo "class='active'"; } ?>><a  href="creerevenement.php">Signaler évenement</a></li>
                 <li class="dropdown">
                     <a <?php if($_SESSION["page"]=="infos.php"){ echo "class='active'"; } ?> href="infos.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Infos<span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a <?php if($_SESSION["page"]=="infos_event.php"){ echo "class='active'"; } ?> href="infos.php?type=event">Evenement</a></li>
-                        <li><a <?php if($_SESSION["page"]=="infos_route.php"){ echo "class='active'"; } ?> href="infos.php?type=route">Condition route</a></li>
-                        <li><a <?php if($_SESSION["page"]=="infos_transport.php"){ echo "class='active'"; } ?> href="infos.php?type=transport">Transport</a></li>
+                        <li <?php if($_SESSION["page"]=="infos_event.php"){ echo "class='active'"; } ?>><a  href="infos.php?type=event">Evenement</a></li>
+                        <li <?php if($_SESSION["page"]=="infos_route.php"){ echo "class='active'"; } ?>><a  href="infos.php?type=route">Condition route</a></li>
+                        <li <?php if($_SESSION["page"]=="infos_transport.php"){ echo "class='active'"; } ?>><a  href="infos.php?type=transport">Transport</a></li>
                     </ul>
                 </li>
-                <li><a <?php if($_SESSION["page"]=="formation.php"){ echo "class='active'"; } ?> href="formation.php">Formation</a></li>
+                <li <?php if($_SESSION["page"]=="formation.php"){ echo "class='active'"; } ?>><a  href="formation.php">Formation</a></li>
                 <li class="dropdown">
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><button type="submit" class="btn btn-primary btn-sm"><a <?php if($_SESSION["page"]=="connexion.php"){ echo "class='active'"; } ?> href="connexion.php">Connexion</a></button></li>
+                <li <?php if($_SESSION["page"]=="connexion.php"){ echo "class='active'"; } ?>><button type="submit" class="btn btn-primary btn-sm"><a  href="connexion.php">Connexion</a></button></li>
             </ul>
 
         </div><!--/.nav-collapse -->
--- index.php
+++ index.php
@@ -1,42 +1,13 @@
+<?php
+session_start();
+$_SESSION["page"]="index.php";
+?>
 <!DOCTYPE html>
 <html lang='fr'>
 <?php include_once("includes/_head.php"); ?>
+<?php include_once("includes/_header.php"); ?>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Cookies Prévention</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="soiree.php">Creer soirée</a></li>
-                <li><a href="event.php">Signaler event</a></li>
 
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Infos <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Evenement</a></li>
-                        <li><a href="#">Condition route</a></li>
-                        <li><a href="#">Transport</a></li>
-                    </ul>
-                </li>
-                <li><a href="formation.php">Formation</a></li>
-                <li class="dropdown">
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a class="connect" href="connexion.php">Connexion</a></li>
-            </ul>
-
-        </div><!--/.nav-collapse -->
-
-    </div>
-</nav>
 
 <!-- COOKIES ! -->
 <body>
