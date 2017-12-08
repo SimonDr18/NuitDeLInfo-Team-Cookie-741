ac093f5d8f068fb72ffd02633e4f45221b6d8127
GUICHARD Clement <clement.guichard1@etu.univ-orleans.fr>
2017-12-08 02:07:33 +0100

    404 add

---
+++ 404.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>404 error</title>
+    <link rel="stylesheet" href="static/404.css">
+  </head>
+  <body>
+  </body>
+</html>
