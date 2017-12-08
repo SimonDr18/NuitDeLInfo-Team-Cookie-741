375d909794c81e50f642697d3c0d034980b0578a
Heloise Coulon <coulon@info27-20.iut45.univ-orleans.fr>
2017-12-08 05:16:27 +0100

    un VRAI message

--- creerevenement.php
+++ creerevenement.php
@@ -4,23 +4,45 @@ $_SESSION["page"]="creerevenement.php";
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8" />
     <title>Créer un événement</title>
     <?php require_once("includes/_head.php"); ?>
-  </head>
-  <body>
-    <?php require_once("includes/_header.php"); ?>
-    <header>
-      <h1>Signaler un evenement</h1>
-    </header>
-    <section>
-      <form action="#" method="post">
-        Titre evenement : <input type="text" name="event" /><br />
-        Type : <input type="text" name="type" /><br />
-        Nombre de blessés : <input type="number" name="nbblesses" /><br />
-        <input type="submit" value="Creer"/>
-      </form>
-    </section>
-  </body>
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <?php require_once("includes/_header.php"); ?>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1>Signaler un évènement</h1>
+                </div>
+                <div class="panel-body ajout" >
+                    <div class="ajoutbox">
+                        <form action="#" method="post">
+                            <div class="form-group">
+                                <div class="form-group">
+                                    <label>Titre evenement :</label><input  type="text" name="event" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Type : </label><input  type="text" name="type" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Nombre de blessés : </label><input  type="number" name="nbblesses" />
+                                </div>
+                            </div>
+                            <div>
+                                <input type="submit" value="Créer" class="btn btn-default">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
+
+
